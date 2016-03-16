6a29f4ae153aefbfc7c7c8a98abf73387843155f
aniket <nktdtt8@gmail.com>
2016-03-16 15:53:53 +0530

    initial commit adding all packages

---
+++ core/src/main/java/org/ad/research/entityresolution/core/App.java
@@ -0,0 +1,13 @@
+package org.ad.research.entityresolution.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
